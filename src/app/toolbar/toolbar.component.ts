582331a0732ebd0c2932aeb910ea9a12ad4e0a09
AGmaj <adrian.gmaj@gmail.com>
2024-02-16 18:05:07 +0100

    toolbar scrolled new

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -15,17 +15,14 @@ export class ToolbarComponent implements OnInit {
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
     let element = document.querySelector('.nav') as HTMLElement;
-    if (this.router.url === '/') {
+
 
       if (window.scrollY > element.clientHeight * 3) {
         element.classList.add('scrolled');
       } else {
         element.classList.remove('scrolled');
       }
-    } else {
 
-      element.classList.add('scrolled');
-    }
   }
 
   openSide(){
